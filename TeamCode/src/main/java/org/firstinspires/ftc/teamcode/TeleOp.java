57af88d3c577ad878d2261a96a551cc513baae14
yearbook <caleb.andrew.flaim@gmail.com>
2019-01-16 15:02:48 -0600

    added new teleop control

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp.java
@@ -24,11 +24,12 @@ public class TeleOp extends OpMode {
 
         telemetry.addData("Initalized","Initialization complete");
     }
+
     @Override
     public void loop() {
 
-        leftDrive.setPower(gamepad1.left_stick_y * 1.5);
-        rightDrive.setPower(gamepad1.right_stick_y * 1.5);
+        leftDrive.setPower(getVelocity(gamepad1.left_stick_y));
+        rightDrive.setPower(getVelocity(gamepad1.right_stick_y));
 
         if(gamepad1.right_trigger > 0.1) {
             winchLift.setPower(gamepad1.right_trigger);
@@ -46,4 +47,26 @@ public class TeleOp extends OpMode {
             tubeArm.setPower(0.0);
         }
     }
+
+    private double lin_map(double value, double in_min, double in_max, double out_min, double out_max) {
+        return (value - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+    private double getVelocity(double value) {
+        double magnitude = Math.abs(value);
+        int direction;
+        if(value > 0) direction = 1;
+        else if(value < 0) direction = -1;
+        else direction = 0;
+
+        if(magnitude > 0.0 && magnitude <= 0.3) {
+            return lin_map(magnitude, 0.0, 0.3, 0.0, 0.2) * direction;
+        } else if(magnitude > 0.3 && magnitude <= 0.6) {
+            return lin_map(magnitude, 0.3, 0.6, 0.2, 0.4) * direction;
+        } else if(magnitude > 0.6) {
+            return lin_map(magnitude, 0.6, 1.0, 0.4, 1.0) * direction;
+        }
+        return 0;
+    }
+
 }
